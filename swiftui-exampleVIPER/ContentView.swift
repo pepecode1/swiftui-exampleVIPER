4311c04570dfdefc63b7d56343360c394af849ca
Antonio Ruiz <pepecode1@gmail.com>
2025-09-25 10:21:10 -0600

    Initial Commit

---
+++ swiftui-exampleVIPER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-exampleVIPER
+//
+//  Created by Pepe Ruiz on 25/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-exampleVIPER/swiftui_exampleVIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_exampleVIPERApp.swift
+//  swiftui-exampleVIPER
+//
+//  Created by Pepe Ruiz on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_exampleVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
